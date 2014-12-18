58e01ce4cd9c6ff6c19c29d03f0df35d2c4d0a21
Damien Collard <damien.collard@ubeeko.com>
2014-12-18 16:28:53 +0100

    Fix use of delimMarker before it's defined (hello, null) :-/

--- src/main/scala/starschema/slm4j/Delim.scala
+++ src/main/scala/starschema/slm4j/Delim.scala
@@ -1,12 +1,12 @@
 package starschema.slm4j
 
 object Delim {
+    val delimMarker = "-----"
+
+    private def delim(s: String) = s"$delimMarker $s $delimMarker"
+
     val LICENSE_BEGIN   = delim("BEGIN LICENSE")
     val LICENSE_END     = delim("END LICENSE")
     val SIGNATURE_BEGIN = delim("BEGIN SIGNATURE")
     val SIGNATURE_END   = delim("END SIGNATURE")
-
-    private def delim(s: String) = s"$delimMarker $s $delimMarker"
-
-    val delimMarker = "-----"
 }
