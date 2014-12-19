849166f9a3b9f93e4b6497cd655910399bdb5e0a
Damien Collard <damien.collard@ubeeko.com>
2014-12-19 16:18:00 +0100

    Add unit test for Delim.

---
+++ src/test/scala/org/distfp/sts/DelimSpec.scala
@@ -0,0 +1,25 @@
+package org.distfp.sts
+
+import org.specs2.mutable.Specification
+
+import Delim._
+
+class DelimSpec extends Specification {
+  val testMarkers = Seq("", "dummy", "THING With SpACeS")
+
+  "beginDelim" should {
+    "return the provided string prefixed with BEGIN and surrounded by separators" in {
+       testMarkers forall { s =>
+        beginDelim(s) must_== s"$delimSeparator BEGIN $s $delimSeparator"
+      }
+    }
+  }
+
+  "endDelim" should {
+    "return the provided string prefixed with END and surrounded by separators" in {
+      testMarkers forall { s =>
+        endDelim(s) must_== s"$delimSeparator END $s $delimSeparator"
+      }
+    }
+  }
+}
